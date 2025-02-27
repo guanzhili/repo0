7924d4a64578af79969cdf5563864c4bd603a08b
Guan <guanzl@7cf34dec3f2e.ant.amazon.com>
2025-02-27 23:39:58 +0800

    update

--- src/main.go
+++ src/main.go
@@ -2,20 +2,53 @@ package main
 
 import (
 	"fmt"
-)
-
-func countChars(str string) map[rune]int {
-countMap := make(map[rune]int)
-	for _, char := range str {
-		countMap[char]++
+	"log"
+	"os"
+)func main() {
+	people := []Person{
+		{"Alice", 25},
+		{"Bob", 17},
 	}
-	return countMap
-}
-func main() {
-str := "hello world"
-	result := countChars(str)
 
-	for char, count := range result {
-		fmt.Printf("字符 '%c' 出现了 %d 次\n", char, count)
+	for _, person := range people {
+		person.Print()
+		fmt.Println(person.Greet())
 	}
+
+	// Reading a file
+	fileContent := readFile("example.txt")
+	fmt.Println("File content:", fileContent)
 }
+
+type Person struct {
+	Name string
+	Age  int
+}
+
+func (p Person) Print() {
+	fmt.Println(p.Name, p.Age)
+}
+
+func (p Person) Greet() string {
+	if p.Age > 18 {
+		return fmt.Sprintf("Hello, %s!", p.Name)
+	}
+	return fmt.Sprintf("Hey, %s!", p.Name)
+}
+
+func readFile(fileName string) string {
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal("Error opening file", err)
+	}
+	defer file.Close()
+
+	var content string
+	_, err = fmt.Fscanf(file, "%s", &content)
+	if err != nil {
+		log.Fatal("Error reading file", err)
+	}
+	return content
+}
+
+
---
+++ src/util/util.go
@@ -0,0 +1,23 @@
+package util
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func countChars(str string) map[rune]int {
+	countMap := make(map[rune]int)
+	for _, char := range str {
+		countMap[char]++
+	}
+	return countMap
+}
+
+func countChars(str string) map[rune]int {
+	countMap := make(map[rune]int)
+	for _, char := range str {
+		countMap[char]++
+	}
+	return countMap
+}
